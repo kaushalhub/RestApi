a11838d7f50767d4631d37630ff5897cd6847b00
kaushalhub <40087944+kaushalhub@users.noreply.github.com>
2020-04-30 17:02:53 +0530

    dasd

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,31 @@
 import React, { Fragment } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from  './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import Alert from './components/layout/Alert';
 import './App.css';
-
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
 const  App = () => {
   return (
+  <Provider store={store}>
+   <Router> 
     <Fragment>
-        <h1>App</h1>
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <section className="container">
+        <Alert />
+          <Switch>
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </section>
     </Fragment>
+   </Router> 
+  </Provider> 
   );
 }
 
